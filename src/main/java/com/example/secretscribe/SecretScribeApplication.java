a0a4902a67408111bc957d2ee6100372ceb4632b
Nikola Arsov <arsovnikola1@gmail.com>
2024-04-26 20:49:54 +0200

    intial project

---
+++ src/main/java/com/example/secretscribe/SecretScribeApplication.java
@@ -0,0 +1,13 @@
+package com.example.secretscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretScribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretScribeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secretscribe/SecretScribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secretscribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretScribeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
