f2af06522c55a23ce37ca406c8a4fba55ccc3d8d
Nikola Arsov <arsovnikola1@gmail.com>
2024-05-14 21:29:48 +0200

    added error handling for empty confession

--- src/main/java/com/example/secretscribe/web/controller/ConfessionController.java
+++ src/main/java/com/example/secretscribe/web/controller/ConfessionController.java
@@ -3,7 +3,9 @@ package com.example.secretscribe.web.controller;
 import com.example.secretscribe.model.Confession;
 import com.example.secretscribe.model.exceptions.ConfessionIsEmptyException;
 import com.example.secretscribe.service.ConfessionService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,13 @@ public class ConfessionController {
     }
 
     @GetMapping(path = "/add")
-    public String getAddConfessionPage()
+    public String getAddConfessionPage(@RequestParam(required = false) String error, Model model)
     {
+        if(error!=null && !error.isEmpty())
+        {
+            model.addAttribute("hasError",true);
+            model.addAttribute("error",error);
+        }
         return "leaveConfession";
     }
 
@@ -33,13 +40,14 @@ public class ConfessionController {
     {
         if(text.isEmpty())
         {
-            throw new ConfessionIsEmptyException();
+            //throw new ConfessionIsEmptyException();
+            return "redirect:/confession/add?error=ConfessionTextIsEmpty";
         }
         confessionService.save(text);
         return "redirect:/confession/add/success";
     }
 
-    @GetMapping
+    @GetMapping("/add/success")
     public String getAddSuccessPage()
     {
         return "confessionAddedSuccess";
